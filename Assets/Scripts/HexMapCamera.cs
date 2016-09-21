96d4f1671c4a00edb60ff009c9dea21b2421037e
oguzunal <oguzunal89@gmail.com>
2016-09-21 22:40:16 +0300

    added camera rotation

--- Assets/Scripts/HexMapCamera.cs
+++ Assets/Scripts/HexMapCamera.cs
@@ -11,6 +11,10 @@ public class HexMapCamera : MonoBehaviour {
     //public float moveSpeed;
     public float moveSpeedMinZoom, moveSpeedMaxZoom;
 
+    public float rotationSpeed;
+
+    float rotationAngle;
+
     public HexGrid grid;
 
     void Awake() {
@@ -24,6 +28,11 @@ public class HexMapCamera : MonoBehaviour {
             AdjustZoom(zoomDelta);
         }
 
+        float rotationDelta = Input.GetAxis("Rotation");
+        if(rotationDelta != 0f) {
+            AdjustRotation(rotationDelta);
+        }
+
         float xDelta = Input.GetAxis("Horizontal");
         float zDelta = Input.GetAxis("Vertical");
 
@@ -33,7 +42,7 @@ public class HexMapCamera : MonoBehaviour {
     }
 
     void AdjustPosition(float xDelta, float zDelta) {
-        Vector3 direction = new Vector3(xDelta, 0f, zDelta).normalized;
+        Vector3 direction = transform.localRotation * new Vector3(xDelta, 0f, zDelta).normalized;
         float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
         float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom) * damping * Time.deltaTime;
 
@@ -50,6 +59,19 @@ public class HexMapCamera : MonoBehaviour {
         position.z = Mathf.Clamp(position.z, 0f, zMax);
 
         return position;
+    }
+
+    void AdjustRotation(float delta) {
+        rotationAngle += delta * rotationSpeed * Time.deltaTime;
+
+        if (rotationAngle < 0) {
+            rotationAngle += 360f;
+        }
+        else if(rotationAngle >= 360f) {
+            rotationAngle -= 360f;
+        }
+
+        transform.localRotation = Quaternion.Euler(0f, rotationAngle, 0f);
     }
 
     void AdjustZoom(float delta) {
