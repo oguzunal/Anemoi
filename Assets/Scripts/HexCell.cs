447f12ae0eb640cb6f03cce52c78bb8d9707fee0
oguzunal <oguzunal89@gmail.com>
2016-09-21 20:07:49 +0300

    step 4 finished

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+    public HexCoordinates coordinates;
+    public Color color;
+
+    private int elevation;
+    public int Elevation {
+        get {
+            return elevation;
+        }
+        set {
+            elevation = value;
+            Vector3 position = transform.localPosition;
+            position.y = value * HexMetrics.elevationStep;
+            position.y += (HexMetrics.SampleNoise(position).y * 2f - 1f) * HexMetrics.elevationPerturbStrength;
+            transform.localPosition = position;
+
+            Vector3 uiPosition = uiRect.localPosition;
+            uiPosition.z = -position.y;
+            uiRect.localPosition = uiPosition;
+        }
+    }
+
+    public Vector3 Position {
+        get {
+            return transform.localPosition;
+        }
+    }
+
+    [SerializeField]
+    HexCell[] neighbours;
+
+    public RectTransform uiRect;
+
+    public HexCell GetNeighbour(HexDirection direction) {
+        return neighbours[(int)direction];
+    }
+
+    public void SetNeighbour(HexDirection direction, HexCell cell) {
+        neighbours[(int)direction] = cell;
+        cell.neighbours[(int)direction.Opposite()] = this;
+    }
+
+    public HexEdgeType GetEdgeType(HexDirection direction) {
+        return HexMetrics.GetEdgeType(elevation, neighbours[(int)direction].elevation);
+    }
+
+    public HexEdgeType GetEdgeType(HexCell otherCell) {
+        return HexMetrics.GetEdgeType(elevation, otherCell.elevation);
+    }
+}
---
+++ Assets/Scripts/HexMapEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour {
+
+    public Color[] colors;
+
+    public HexGrid hexGrid;
+
+    private Color activeColor;
+
+    int activeElevation;
+
+    void Awake() {
+        SelectColor(0);
+    }
+
+    void Update() {
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) {
+            HandleInput();
+        }
+    }
+
+    void HandleInput() {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) {
+            EditCell(hexGrid.GetCell(hit.point));
+        }
+    }
+
+    public void SelectColor(int index) {
+        activeColor = colors[index];
+    }
+
+    public void EditCell(HexCell cell) {
+        cell.color = activeColor;
+        cell.Elevation = activeElevation;
+        hexGrid.Refresh();
+    }
+
+    public void SetElevation(float elevation) {
+        activeElevation = (int)elevation;
+    }
+}
